0170d907dc2a2527c9d6ac5ef70f917da65f7d32
Utkarsh Banka <93452877+utkarshbank@users.noreply.github.com>
2023-09-02 21:28:28 -0700

    Add files via upload

---
+++ extension.py
@@ -0,0 +1,16 @@
+Extention_name = input("Please enter your file name: ")
+split_extention_name = Extention_name.split('.')
+if split_extention_name[1] == "gif":
+    print("Image/gif")
+elif split_extention_name[1] == "jpg":
+    print("Image/jpg")
+elif split_extention_name[1] == "jpeg":
+    print("Image/jpg")
+elif split_extention_name[1] == "txt":
+    print("Text/txt")
+elif split_extention_name[1] == "mp3":
+    print("Music/mp3")
+elif split_extention_name[1] == "mp4":
+    print("Videos/mp4")
+else:
+    print("Aplication not found")
